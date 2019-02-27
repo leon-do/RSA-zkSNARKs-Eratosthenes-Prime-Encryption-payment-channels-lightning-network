1bc47610a2ece427ce0d087b6e24aa3408922aa2
Leon Do <leondo123@gmail.com>
2019-02-27 02:01:42 -0700

    Update RSA.js

--- RSA.js
+++ RSA.js
@@ -1,40 +1,40 @@
 var BigNumber = require('big-number');
 
 //private info. prime numbers
-const p = 17
-const q = 19
+const p = 11
+const q = 17
+console.log('my private info', {p,q})
 
 //public info
 const n = p * q
-const e = 7 	
+const e = 3	
 	// https://brilliant.org/wiki/relatively-prime/
 	// e is an integer
 	// e is not not a factor of n
 	// 1 < e < (p-1)(q-1)
+console.log('my public info', {e, n})
+
+/*
+find d, the private key
+find the modular inverse d of e modulo ((p-1)*(q-1))
+(d * e) mod ((p-1)*(q-1)) === 1
+*/
+let d = 1
+while ((d * e) % ((p-1)*(q-1)) !== 1) {
+	d++
+}
+console.log('my private key', {d})
+
 
 // the message
 const message = 72
+console.log('users creates message', message)
 
 // encrypt message
-// 72^7 % 323 = 13
 const encryptedMessage = Math.pow(message,e) % n
-console.log(encryptedMessage) // 13
-
-// find d
-// 7 * d % (p-1)(q-1) = 1
-let d = 1
-while ( (7 * d) % ((p-1)*(q-1)) !== 1 ){
-	d++
-}
-console.log(d) // 247
+console.log('user encrypts message with my public key', {encryptedMessage})
 
 
 //decrypt message
-// 13^247 % 323
 const decryptedMessage = BigNumber(encryptedMessage).pow(d).mod(n)
-
-// 72
-console.log(Number(decryptedMessage.number.reverse().join('')))
-
-
-
+console.log('i use priv key to decrypt message', {decryptedMessage: Number(decryptedMessage.number.reverse().join(''))})
